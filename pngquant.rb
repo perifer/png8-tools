9f1c57ae5d2679851794505c7a8d76a4e75e43c2
Anders Fajerson <anders@perifer.se>
2011-10-23 22:36:19 +0200

    Homebrew formulas for pngquant and pngout.

---
+++ pngquant.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Pngquant < Formula
+  url 'http://pornel.net/pngquant/pngquant.tar.bz2'
+  homepage 'http://pornel.net/pngquant'
+  md5 '4b1e996cccd58498ff1ecb9b1ae60ebf'
+  @version='1.5'
+
+  # Stripping results in malformed object error.
+  skip_clean 'bin/pngquant'
+
+  def install
+    mv 'pngquant-mac', 'pngquant'
+    bin.install 'pngquant'
+  end
+
+end
