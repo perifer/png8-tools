dedce1aca3c39e132e111c3bbd68f6772cddf12b
Anders Fajerson <perifer@gmail.com>
2012-03-16 22:36:27 +0100

    Bump version

--- pngquant.rb
+++ pngquant.rb
@@ -4,7 +4,7 @@ class Pngquant < Formula
   url 'http://pornel.net/pngquant/pngquant.tar.bz2'
   homepage 'http://pornel.net/pngquant'
   md5 '4b1e996cccd58498ff1ecb9b1ae60ebf'
-  @version='1.5'
+  @version='1.7.2'
 
   # Stripping results in malformed object error.
   skip_clean 'bin/pngquant'
