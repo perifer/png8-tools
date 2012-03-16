cc4b7054a155332476a6e40ef66cd63d054b0d67
Anders Fajerson <perifer@gmail.com>
2012-03-16 22:58:46 +0100

    Update url to new download location.

--- pngquant.rb
+++ pngquant.rb
@@ -1,16 +1,15 @@
 require 'formula'
 
 class Pngquant < Formula
-  url 'http://pornel.net/pngquant/pngquant.tar.bz2'
-  homepage 'http://pornel.net/pngquant'
-  md5 '4b1e996cccd58498ff1ecb9b1ae60ebf'
+  url 'http://pngquant.org/pngquant.tar.bz2'
+  homepage 'http://pngquant.org'
+  md5 'cd656350b831dda7aa14301b36beeed8'
   @version='1.7.2'
 
   # Stripping results in malformed object error.
   skip_clean 'bin/pngquant'
 
   def install
-    mv 'pngquant-mac', 'pngquant'
     bin.install 'pngquant'
   end
 
