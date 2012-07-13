f3181aee76f76d387b4b5bbe917cb326865c2527
Oncle Tom <thomas@oncle-tom.net>
2012-07-13 13:31:39 +0300

    Fixing @version

--- pngquant.rb
+++ pngquant.rb
@@ -4,7 +4,7 @@ class Pngquant < Formula
   url 'http://pngquant.org/pngquant.tar.bz2'
   homepage 'http://pngquant.org'
   md5 'cd656350b831dda7aa14301b36beeed8'
-  @version='1.7.2'
+  version '1.7.2'
 
   # Stripping results in malformed object error.
   skip_clean 'bin/pngquant'
